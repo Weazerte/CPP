6c22a7517a15eec7734f6c3a202eba22446490ec
E'ouann AUBRY <eaubry@bess-f4r3s5.clusters.42paris.fr>
2024-01-16 18:39:56 +0100

    cpp 01 in progress

--- CPP01/ex00/Zombie.hpp
+++ CPP01/ex00/Zombie.hpp
@@ -6,7 +6,33 @@
 /*   By: eaubry <eaubry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 17:45:22 by eaubry            #+#    #+#             */
-/*   Updated: 2024/01/16 17:45:23 by eaubry           ###   ########.fr       */
+/*   Updated: 2024/01/16 18:36:45 by eaubry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ZOMBIE_HPP
+
+# define ZOMBIE_HPP
+
+#include <iostream>
+# include <iostream>
+# include <string>
+# include <limits>
+# include <csignal>
+# include <cstdlib>
+# include <cstring>
+# include <iomanip>
+
+class Zombie
+{
+	private:
+		std::string name;
+	public:
+		Zombie(void);
+		~Zombie(void);
+		void SetName(std::string name);
+		void announce( void ) const ;
+		
+}
+
+#endif
--- CPP01/ex00/newZombie.cpp
+++ CPP01/ex00/newZombie.cpp
@@ -1,27 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*   newZombie.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eaubry <eaubry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/16 17:41:26 by eaubry            #+#    #+#             */
-/*   Updated: 2024/01/16 18:38:33 by eaubry           ###   ########.fr       */
+/*   Created: 2024/01/16 18:23:14 by eaubry            #+#    #+#             */
+/*   Updated: 2024/01/16 18:25:34 by eaubry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie::Zombie(void){}
-
-Zombie::~Zombie(void){}
-
-void	Zombie::SetName(std::string name)
+Zombie *newZombie(std::string name)
 {
-	this->name = name;
-}
-
-void	Zombie::announce() const
-{
-	std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+	Zombie *zombie;
+	zombie.name = name;
+	return (zombie);
 }
